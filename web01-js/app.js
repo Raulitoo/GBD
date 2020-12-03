0f10eb9f4d5c6b283927cf550263d4152b7a1891
Raulitoo <140238@iessierradeguara.com>
2020-12-03 16:36:01 +0100

    primeros_pasos

---
+++ web01-js/app.js
@@ -0,0 +1,25 @@
+console.log("Hola mundo");
+
+let texto = "Esto es un texto";
+let numero = 25;
+let decimal = 5.4;
+let booleano = true;
+
+let array = ["jose", 58, "Moreno"];
+
+let Persona = {
+    "nombre": "Manuel",
+    "edad": 23,
+    "cabello": "Rubio"
+
+}
+
+console.log(texto);
+console.log(numero);
+console.log(decimal);
+console.log(booleano);
+console.log(array);
+console.log(Persona.nombre);
+console.log(Persona["nombre"]);
+console.log(array[1]);
+
